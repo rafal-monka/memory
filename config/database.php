01bb53d9d1934895b7709bdde6ca2c0fb6bac77c
Rafal <rafal.monka.tue@gmail.com>
2018-05-09 20:26:38 +0200

    ENV variables

--- config/database.php
+++ config/database.php
@@ -51,11 +51,11 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'mysql'),
-			'port'      => env('DB_PORT', 3306),
-			'database'  => env('DB_DATABASE', 'default'),
-			'username'  => env('DB_USERNAME', 'laravel'),
-			'password'  => env('DB_PASSWORD', 'rafal'),
+			'host'      => env('DB_HOST'),
+			'port'      => env('DB_PORT'),
+			'database'  => env('DB_DATABASE'),
+			'username'  => env('DB_USERNAME'),
+			'password'  => env('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
