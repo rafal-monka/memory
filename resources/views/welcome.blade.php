54726abfe057c93f0fa38c34867367e8c5f61b1c
Rafał Mońka <monka.rafal@gmail.com>
2018-11-30 21:23:24 +0100

    copy Current year

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
 
     vertical-align:middle;
 
-    opacity: 1;">&copy;2018/<script>document.write((new Date()).getFullYear());</script></div>                                                    
+    opacity: 1;">&copy;<script>document.write((new Date()).getFullYear())</script></div>                                                    
 
             </div>
 
